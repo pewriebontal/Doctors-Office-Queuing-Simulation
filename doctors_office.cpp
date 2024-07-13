bc488fb6d1716c9d53ee3868f8a5c2f7b5bfd80e
Pewrie Bontal <0x@bontal.net>
2024-07-14 03:41:21 +0500

    implemented doctors_office.cpp

---
+++ doctors_office.cpp
@@ -0,0 +1,62 @@
+#include "doctors_office.h"
+
+doctors_office::doctors_office(int num_docs)
+{
+	doctors.resize(num_docs);
+}
+
+doctors_office::~doctors_office() { }
+
+int doctors_office::get_free_doctor_ID() const
+{
+	for(int i = 0; i < num_doctors; i++)
+	{
+		if(doctors[i].is_free())
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int doctors_office::get_number_of_busy_doctors() const
+{
+	int count = 0;
+	for(int i = 0; i < num_doctors; i++)
+	{
+		if(!doctors[i].is_free())
+		{
+			count++;
+		}
+	}
+	return count;
+}
+
+void doctors_office::set_doctor_busy(int doc_ID, const patient& pat, int app_time)
+{
+	doctors[doc_ID].set_busy();
+	doctors[doc_ID].set_current_patient(pat);
+	doctors[doc_ID].set_remaining_appointment_time(app_time);
+}
+
+void doctors_office::set_doctor_busy(int doc_ID, const patient& pat)
+{
+	doctors[doc_ID].set_busy();
+	doctors[doc_ID].set_current_patient(pat);
+	doctors[doc_ID].set_remaining_appointment_time();
+}
+
+void doctors_office::update_doctors(int time)
+{
+	for(int i = 0; i < num_doctors; i++)
+	{
+		if(!doctors[i].is_free())
+		{
+			doctors[i].decrement_remaining_appointment_time();
+			if(doctors[i].get_remaining_appointment_time() == 0)
+			{
+				doctors[i].set_free();
+			}
+		}
+	}
+}
